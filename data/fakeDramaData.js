70fa5942d33646b88d2e12199da71f1f77d64e35
Paula de Jaime <pauladejaime@gmail.com>
2022-08-11 12:45:30 +0200

    Create fakeDramaData.js

---
+++ data/fakeDramaData.js
@@ -0,0 +1,86 @@
+export const drama = () => {
+  return {
+     "name": "Extraordinary Attorney Woo",
+          "image": "/xefsQ5rz9PWN8mO7UZLoiPk9nuV.jpg",
+          "countries": [
+              "KR"
+          ],
+          "episodeCount": 16,
+          "recommendations": [
+              {
+                  "adult": false,
+                  "backdrop_path": "/sVeh7ziCdgdCq8l0Lx4vdOwFyNV.jpg",
+                  "id": 157236,
+                  "name": "Becoming Witch",
+                  "original_language": "ko",
+                  "original_name": "마녀는 살아있다",
+                  "overview": "Three female friends, who come from totally different backgrounds, encounter enemies that they want to kill.",
+                  "poster_path": "/3gQ8OTG1AbtBsQbAHIe1z9qcv63.jpg",
+                  "media_type": "tv",
+                  "genre_ids": [
+                      18,
+                      9648
+                  ],
+                  "popularity": 4.93,
+                  "first_air_date": "2022-06-25",
+                  "vote_average": 5,
+                  "vote_count": 2,
+                  "origin_country": [
+                      "KR"
+                  ]
+              },
+              {
+                  "adult": false,
+                  "backdrop_path": "/omY2At0U5zPDYnhxPLD79ZfZIXO.jpg",
+                  "id": 138663,
+                  "name": "Café Minamdang",
+                  "original_language": "ko",
+                  "original_name": "미남당",
+                  "overview": "A suspicious business that offers the services of a purportedly all-knowing shaman catches the attention of a tenacious police inspector.",
+                  "poster_path": "/jFXSdUd12KzpIKixEmTL0P7osxP.jpg",
+                  "media_type": "tv",
+                  "genre_ids": [
+                      18
+                  ],
+                  "popularity": 36.779,
+                  "first_air_date": "2022-06-27",
+                  "vote_average": 7.7,
+                  "vote_count": 6,
+                  "origin_country": [
+                      "KR"
+                  ]
+              },
+              {
+                  "adult": false,
+                  "backdrop_path": "/9K9ZiwcHDzP3Pchy32EQuUNPVzB.jpg",
+                  "id": 196268,
+                  "name": "Anna",
+                  "original_language": "ko",
+                  "original_name": "안나",
+                  "overview": "It all starts with a simple lie. Lee Yu-mi takes on a privileged young woman's identity, leading her to a completely different life. When she marries a wealthy man whose ambitions gradually put them in the public eye, her perfectly-built persona begins to unravel.",
+                  "poster_path": "/2Dq9A4l5KaoBz1LerrDcBVgHdJm.jpg",
+                  "media_type": "tv",
+                  "genre_ids": [
+                      18
+                  ],
+                  "popularity": 26.275,
+                  "first_air_date": "2022-06-24",
+                  "vote_average": 7.9,
+                  "vote_count": 9,
+                  "origin_country": [
+                      "KR"
+                  ]
+              }
+          ],
+          "genres": [
+              {
+                  "id": 18,
+                  "name": "Drama"
+              }
+          ],
+          "voteAverage": 9,
+          "firstAirDate": "2022-06-29",
+          "plot": "Brilliant attorney Woo Young-woo tackles challenges in the courtroom and beyond as a newbie at a top law firm and a woman on the autism spectrum.",
+          "watchLink": "http://ena.skylifetv.co.kr/bbs/board.php?bo_table=skydrama&wr_id=113&sca=최신"
+  };
+};
